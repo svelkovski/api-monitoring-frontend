9c057b21ab0a3374689b17a3190fea3fc7a10369
svelkovski <appsmailmehere@gmail.com>
2026-04-04 21:28:33 +0200

    Implement authentication service and components with routing
    * Add authentication dtos
    
    * Set up auth service and environment urls
    
    * Initialize login and register components
    
    * Implement routing for login and register components
    
    * Add HTTP interceptor for JWT token handling
    
    * Add auth guard to protect routes
    
    * Implement login and registration components with form validation and error handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
+    }
+];
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { RegisterResponse } from '../model/register-response.model';
+import { RegisterRequest } from '../model/register-request.model';
+import { Observable, tap } from 'rxjs';
+import { LoginRequest } from '../model/login-request.model';
+import { LoginResponse } from '../model/login-response.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = `${environment.apiUrl}/auth`;
+  private httpClient = inject(HttpClient);
+
+  register(data: RegisterRequest): Observable<RegisterResponse> {
+    return this.httpClient.post<RegisterResponse>(
+      `${this.apiUrl}/register`,
+      data,
+    );
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  login(data: LoginRequest): Observable<LoginResponse> {
+    return this.httpClient
+      .post<LoginResponse>(`${this.apiUrl}/login`, data)
+      .pipe(tap((response) => this.saveToken(response.token)));
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    const token = this.getToken();
+    if (token) return true;
+    else return false;
+  }
+}
