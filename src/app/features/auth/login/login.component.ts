fda2a59700526820806564aad6a902ecda813eb2
Stefan Velkovski <appsmailmehere@gmail.com>
2026-04-04 21:26:22 +0200

    Implement login and registration components with form validation and error handling

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
+import { LoginRequest } from '../../../core/model/login-request.model';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  loading: boolean = false;
 
+  form = new FormGroup({
+    username: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  error = '';
+
+  submit() {
+    if (this.form.invalid) return;
+
+    this.loading = true;
+    this.error = '';
+
+    const credentials: LoginRequest = {
+      username: this.form.value.username!,
+      password: this.form.value.password!,
+    };
+
+    this.authService.login(credentials).subscribe({
+      next: () => {
+        this.router.navigate(['/dashboard']);
+      },
+      error: () => {
+        this.error = 'Invalid username or password';
+        this.loading = false;
+      },
+    });
+  }
+
+  get usernameIsInvalid() {
+    return (
+      this.form.controls.username.touched &&
+      this.form.controls.username.dirty &&
+      this.form.controls.username.invalid
+    );
+  }
+
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
 }
